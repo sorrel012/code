e7d02ada299d789648943a21cc3c17e9b8c62f8a
hyowonhan <wain1719@gmail.com>
2023-02-02 15:47:14 +0900

    feat: create practice whitespace class

---
+++ JavaTest/src/com/test/java/Ex04_Variable.java
@@ -0,0 +1,22 @@
+package com.test.java;
+
+public class Ex04_Variable {
+    public static void main(String[] args) {
+	// ';' 문장 종결자
+	// 엔터(개행문자), 탭, 스페이스 > 공백문자(whiteSpace)
+	// 자바는 공백문자의 영향을 받지 않는다.
+
+	// 국어
+	int kor = 100;
+
+	// 영어
+	int eng = 90;
+
+	// 수학
+	int math = 80;
+
+	int kor1 = 100; // 국어
+	int eng1 = 90;	// 영어
+	int math1 = 80; // 수학
+    }
+}
