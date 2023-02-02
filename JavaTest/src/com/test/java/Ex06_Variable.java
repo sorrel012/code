db009581180ea8e94700972c2c680aa1bdc6504e
hyowonhan <wain1719@gmail.com>
2023-02-02 17:43:46 +0900

    feat: create task class

---
+++ JavaTest/src/com/test/java/Ex06_Variable.java
@@ -0,0 +1,17 @@
+package com.test.java;
+
+public class Ex06_Variable {
+    public static void main(String[] args) {
+	//과제
+	//- 9개의 자료형
+	//- 값형(8개) + 참조형(1개)
+	// - 변수 생성 + 데이터 + 출력
+
+	//1. 주위의 데이터 선별
+	//2. 데이터 성격 파악 > 자료형(크기)
+	//3. 변수 생성
+	//4. 값 대입 > 상수
+	//5. 출력
+
+    }
+}
