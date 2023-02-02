1900b23ac1de40f558a3247a5526041087d2fb8f
hyowonhan <wain1719@gmail.com>
2023-02-02 15:18:17 +0900

    feat: practice declaration, initialization variable

---
+++ JavaTest/src/com/test/java/Ex03_Variable.java
@@ -0,0 +1,11 @@
+package com.test.java;
+
+public class Ex03_Variable {
+	public static void main(String[] args) {
+		int number;
+		number = 100;
+		number = 95;
+
+		System.out.println(95);
+	}
+}
