ff7ce66a1094097a3751db1fe38bd42d3437cfe4
hyowonhan <wain1719@gmail.com>
2023-02-03 12:34:27 +0900

    feat: create menu

--- JavaTest/src/com/test/java/Ex09_Output.java
+++ JavaTest/src/com/test/java/Ex09_Output.java
@@ -29,5 +29,27 @@ public class Ex09_Output {
 
 	System.out.printf("금액 : %d원\n", price);
 	System.out.printf("금액 : %,d원\n", price);
+	System.out.println();
+
+	// 전체 너비 20칸 + 천단위 표기 + 소수점 아래 2자리까지
+	double num3 = 1234567.89012345;
+
+	System.out.printf("%,20.2f\n", num3);
+	System.out.println();
+	System.out.println();
+	System.out.println();
+
+	//메뉴판 출력
+
+	// 주의!!! 숫자를 출력할 땐 반드시 단위를 표기해야 함.
+	System.out.println("======================");
+	System.out.println("       음료 가격");
+	System.out.println("======================");
+	System.out.printf("콜라:\t\t%,6d\n", 2500);
+	System.out.printf("사이다:\t\t%,6d\n", 3500);
+	System.out.printf("아메리카노:\t\t%,6d\n", 5000);
+	System.out.printf("케이크:\t\t%,6d\n", 12000);
+
+
     }
 }
