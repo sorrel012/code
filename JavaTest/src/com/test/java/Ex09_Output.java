8eb39f0b9745eafe002b966069f1c659e4728584
hyowonhan <wain1719@gmail.com>
2023-02-03 12:21:41 +0900

    feat: add codes about %num1.num2f

--- JavaTest/src/com/test/java/Ex09_Output.java
+++ JavaTest/src/com/test/java/Ex09_Output.java
@@ -2,11 +2,25 @@ package com.test.java;
 
 public class Ex09_Output {
     public static void main(String[] args) {
+
+	//숫자 + 형식문자 > 정렬
 	int num = 123;
 
 	System.out.printf("[%d]\n", num);
 	System.out.printf("[%10d]\n", num);
 	System.out.printf("[%-10d]\n", num);
+	System.out.println();
 
+
+	// %.숫자f
+	double num2 = 3.581;
+
+	System.out.println(num2);
+	System.out.printf("%f\n", num2);
+	System.out.printf("%.2f\n", num2);
+	System.out.printf("%.1f\n", num2);
+	System.out.printf("%.0f\n", num2);
+
+	System.out.printf("%10.1f\n", num2);
     }
 }
