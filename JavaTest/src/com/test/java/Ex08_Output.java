e6fe0236d0693fc63ae22f2375a78d124a878da8
hyowonhan <wain1719@gmail.com>
2023-02-03 10:49:43 +0900

    docs: add codes about println

---
+++ JavaTest/src/com/test/java/Ex08_Output.java
@@ -0,0 +1,11 @@
+package com.test.java;
+
+public class Ex08_Output {
+    public static void main(String[] args) {
+	//System.out.println();
+	System.out.println(100);
+	System.out.println(200);
+
+    }
+
+}
