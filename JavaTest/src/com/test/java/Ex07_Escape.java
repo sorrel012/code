0100f3f8bbfac41451d3c2c663712ba119339800
hyowonhan <wain1719@gmail.com>
2023-02-03 09:32:30 +0900

    feat: add code about \n

---
+++ JavaTest/src/com/test/java/Ex07_Escape.java
@@ -0,0 +1,10 @@
+package com.test.java;
+
+public class Ex07_Escape {
+    public static void main(String[] args) {
+	//1. \n
+	String msg = "안녕하세요.\n홍길동입니다.";
+
+	System.out.println(msg);
+    }
+}
