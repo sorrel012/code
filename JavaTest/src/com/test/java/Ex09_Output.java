8200a505403a665be82b782768410527a845e817
hyowonhan <wain1719@gmail.com>
2023-02-03 12:08:53 +0900

    feat: create printf extension class

---
+++ JavaTest/src/com/test/java/Ex09_Output.java
@@ -0,0 +1,12 @@
+package com.test.java;
+
+public class Ex09_Output {
+    public static void main(String[] args) {
+	int num = 123;
+
+	System.out.printf("[%d]\n", num);
+	System.out.printf("[%10d]\n", num);
+	System.out.printf("[%-10d]\n", num);
+
+    }
+}
