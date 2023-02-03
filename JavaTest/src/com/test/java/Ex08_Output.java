2545e1fd5cbac745c578e231274fc52f2827fcc2
hyowonhan <wain1719@gmail.com>
2023-02-03 11:44:44 +0900

    feat: add printf methods

--- JavaTest/src/com/test/java/Ex08_Output.java
+++ JavaTest/src/com/test/java/Ex08_Output.java
@@ -42,6 +42,32 @@ public class Ex08_Output {
 
 	System.out.println();
 
+	String name = "홍길동";
+	System.out.printf("안녕하세요. %s님. 안녕히 가세요. %s님.\n", name, name);
+
+	//요구사항] insert into tblMember (seq, name, age, address, tel) values (1, '홍길동', 20, '서울시 강남구 역삼동', '010-1234-5678');
+	name = "홍길동";
+	String seq = "1";
+	String age = "20";
+	String address = "서울시 강남구 역삼동";
+	String tel = "010-1234-5678";
+
+	System.out.printf("insert into tblMember (seq, name, age, address, tel) values (%s, '%s', %s, '%s', '%s')\n", seq, name, age, address, tel);
+	System.out.println();
+
+	int a = 100;
+	double b = 3.14;
+	char c = 'A';
+	boolean d = true;
+
+	System.out.printf("문자열: %s\n", name);
+	System.out.printf("정수: %d\n", a);
+	System.out.printf("실수: %f\n", b);
+	System.out.printf("문자: %c\n", c);
+	System.out.printf("논리: %b\n", d);
+
+	System.out.println();
+
 
     }
 
