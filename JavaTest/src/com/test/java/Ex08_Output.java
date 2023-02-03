fa06bc2f26ff12b2e77c607144cf71eb0ab70e54
hyowonhan <wain1719@gmail.com>
2023-02-03 11:05:35 +0900

    feat: add print methods

--- JavaTest/src/com/test/java/Ex08_Output.java
+++ JavaTest/src/com/test/java/Ex08_Output.java
@@ -6,6 +6,12 @@ public class Ex08_Output {
 	System.out.println(100);
 	System.out.println(200);
 
+	//System.out.print();
+	System.out.print("하나");
+	System.out.print("둘");
+
+	System.out.println();
+
     }
 
 }
