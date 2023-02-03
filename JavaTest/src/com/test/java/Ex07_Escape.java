50ae3241b4ca03f4ddc1a778ae2e0c4be7c9608d
hyowonhan <wain1719@gmail.com>
2023-02-03 09:37:27 +0900

    feat: add code about \r

--- JavaTest/src/com/test/java/Ex07_Escape.java
+++ JavaTest/src/com/test/java/Ex07_Escape.java
@@ -6,5 +6,10 @@ public class Ex07_Escape {
 	String msg = "안녕하세요.\n홍길동입니다.";
 
 	System.out.println(msg);
+
+	//2. \r
+	msg = "안녕하세요.\r홍길동";
+
+	System.out.println(msg); //이클립스 콘솔창에서는 \n과 동일하게 동작
     }
 }
