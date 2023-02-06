9cfdab19f0f18148fbb18d5542f797eb2f4c0f24
hyowonhan <wain1719@gmail.com>
2023-02-06 09:26:50 +0900

    feat: copy value

---
+++ JavaTest/src/com/test/java/Ex15_Casting.java
@@ -0,0 +1,19 @@
+package com.test.java;
+
+public class Ex15_Casting {
+    public static void main(String[] args) {
+
+	int n1;
+	int n2;
+
+	n1 = 100; // 100이라는 데이터를 n1이라는 공간에 넣어라(오른쪽에서 왼쪽으로 할당)
+	n2 = n1; // n1이라는 공간의 데이터를 가져와서, n2라는 공간에 넣어라(변수끼리의 복사)
+
+	System.out.println(n2);
+
+
+	// 암시적 형변환
+
+	// 명시적 형변환
+    }
+}
