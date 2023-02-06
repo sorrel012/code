b946b7436d0844ba69d57ce076b14e463a28ff2a
hyowonhan <wain1719@gmail.com>
2023-02-06 11:45:00 +0900

    feat: add casting codes between primitive type and reference type

--- JavaTest/src/com/test/java/Ex17_Casting.java
+++ JavaTest/src/com/test/java/Ex17_Casting.java
@@ -17,5 +17,27 @@ public class Ex17_Casting {
 
 	System.out.print("두 번째 숫자: ");
 	String input2 = reader.readLine();
+
+	int num1 = Integer.parseInt(input1);
+	int num2 = Integer.parseInt(input2);
+
+	System.out.printf("두 숫자의 합은 %d입니다.\n", num1 + num2);
+
+	//참조형(String) > 값형
+	// - 래퍼클래스/parseXXX()
+
+	byte n1 = Byte.parseByte("10");
+	short n2 = Short.parseShort("10");
+	int n3 = Integer.parseInt("10");
+	long n4 = Long.parseLong("10");
+	float n5 = Float.parseFloat("3.14");
+	double n6 = Double.parseDouble("3.14");
+	boolean n7 = Boolean.parseBoolean("true");
+
+	//값형 > 참조형(String)
+	// - String.valueOf();
+	String s1 = String.valueOf(10);
+	String s2 = String.valueOf(3.14);
+	String s3 = String.valueOf("true");
     }
 }
