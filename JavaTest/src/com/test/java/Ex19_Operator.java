d81c4b8a37b3ac459a9b777c4413ff252b4e4d06
hyowonhan <wain1719@gmail.com>
2023-02-06 16:04:29 +0900

    feat: add logical operators

--- JavaTest/src/com/test/java/Ex19_Operator.java
+++ JavaTest/src/com/test/java/Ex19_Operator.java
@@ -18,17 +18,6 @@ public class Ex19_Operator {
 	System.out.println(a != b);
 	System.out.println();
 
-	//요구사항] 사용자 나이 입력 > 성인(19세 이상) O, X
-
-
-	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-	System.out.print("당신의 나이는 몇 살인가요?: ");
-	String input = reader.readLine();
-	int age = Integer.parseInt(input);
-	System.out.println(age >= 19);
-	System.out.println();
-
 
 	boolean b1 = true;
 	boolean b2 = true;
@@ -48,6 +37,37 @@ public class Ex19_Operator {
 	System.out.println(s1 == s4);
 	System.out.println(s1.equals(s2));
 	System.out.println(s1.equals(s4));
+	System.out.println();
+
+
+	//논리 연산자
+
+	//요구사항] 사용자 나이 입력 > 19세 이상~ 60세 미만
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+	System.out.print("당신의 나이는 몇 살인가요?: ");
+	String input = reader.readLine();
+	int age = Integer.parseInt(input);
+	System.out.println(age >= 19 && age < 60);
+	System.out.println();
+
+	System.out.println(true && true);
+	System.out.println(true && false);
+	System.out.println(false || false);
+	System.out.println(false || true);
+	System.out.println(!true);
+	System.out.println();
+
+	a = 10; //1010
+	b = 3; //0011
+
+	System.out.println(a & b); //0010 > 2
+	System.out.println(a | b); //1011 > 11
+	System.out.println(a ^ b); //1001 > 9
+	System.out.println(true ^ true);
+	System.out.println(true ^ false);
+	System.out.println(false ^ true);
+	System.out.println(false ^ false);
 
     }
 }
