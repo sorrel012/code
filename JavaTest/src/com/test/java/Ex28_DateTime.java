a015a11bd5eb9a1c3dffa707a2a9191f8c812ae3
hyowonhan <wain1719@gmail.com>
2023-02-07 17:22:52 +0900

    feat: practice Date class

---
+++ JavaTest/src/com/test/java/Ex28_DateTime.java
@@ -0,0 +1,24 @@
+package com.test.java;
+
+import java.util.Date;
+
+public class Ex28_DateTime {
+
+    public static void main(String[] args) {
+
+	m1();
+
+
+    } //main
+
+    private static void m1() {
+
+	// Date
+
+	Date date = new Date(); //현재 시각을 생성해서 date라는 변수에 담아라 > 현재 컴퓨터로부터
+
+	System.out.println(date);
+
+    } //m1
+
+}
