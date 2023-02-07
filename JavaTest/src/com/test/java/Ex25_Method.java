e3343e9f8c2c86cd1eb9a119fc97f61e181c8f33
hyowonhan <wain1719@gmail.com>
2023-02-07 12:29:27 +0900

    feat: practices method auto creation

--- JavaTest/src/com/test/java/Ex25_Method.java
+++ JavaTest/src/com/test/java/Ex25_Method.java
@@ -12,7 +12,35 @@ public class Ex25_Method {
 	String name = "홍길동";
 	hi(name);
 
+	score();
+	score2(100);
+	String result = score3("홍길동");
+
+	aaa();
+
     } //main
+
+    private static void aaa() {
+
+	int num = 10;
+	int sum = 0;
+
+	num++;
+	num *= 2;
+	sum = num + 100;
+
+	System.out.println(sum);
+    }
+
+    private static String score3(String string) {
+	return null;
+    }
+
+    private static void score2(int i) {
+    }
+
+    private static void score() {
+    }
 
     private static void hi(String name) {
 	System.out.println(name + " 안녕~");
