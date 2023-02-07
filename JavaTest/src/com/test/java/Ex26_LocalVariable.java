317a573fcbc8175e5e0983bdc4e82a8a981bbadb
hyowonhan <wain1719@gmail.com>
2023-02-07 14:02:57 +0900

    feat: create class using local variable

---
+++ JavaTest/src/com/test/java/Ex26_LocalVariable.java
@@ -0,0 +1,24 @@
+package com.test.java;
+
+public class Ex26_LocalVariable {
+
+    public static void main(String[] args) {
+
+	int a = 10;
+
+	int c = 20;
+
+	m1();
+
+    } //main
+
+    public static void m1() {
+
+	int b = 20;
+
+	int c = 40;
+
+	System.out.println(c);
+
+    } //m1
+}
