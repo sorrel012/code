84dd2acd7fc659d647729fccc4d82300d9cf3b12
hyowonhan <wain1719@gmail.com>
2023-02-08 21:15:59 +0900

    fix & refactor: revises String value & change the lines

--- JavaTest/src/com/test/question/Q017.java
+++ JavaTest/src/com/test/question/Q017.java
@@ -19,7 +19,7 @@ public class Q017 {
 
     public static void main(String[] args) {
 
-	position("홍길");
+	position("홍길동");
 	position("홍길동", "유재석");
 	position("홍길동", "유재석", "박명수");
 	position("홍길동", "유재석", "박명수", "정형돈");
@@ -31,15 +31,21 @@ public class Q017 {
     }
 
     private static void position(String name1, String name2) {
-	System.out.printf("사원: %s\n대리: %s\n\n", name1, name2);
+	System.out.printf("사원: %s\n", name1);
+	System.out.printf("대리: %s\n\n", name2);
     }
 
     private static void position(String name1, String name2, String name3) {
-	System.out.printf("사원: %s\n대리: %s\n과장: %s\n\n", name1, name2, name3 );
+	System.out.printf("사원: %s\n", name1);
+	System.out.printf("대리: %s\n", name2);
+	System.out.printf("과장: %s\n\n", name3 );
     }
 
     private static void position(String name1, String name2, String name3, String name4) {
-	System.out.printf("사원: %s\n대리: %s\n과장: %s\n대리: %s\n\n", name1, name2, name3, name4);
+	System.out.printf("사원: %s\n", name1);
+	System.out.printf("대리: %s\n", name2);
+	System.out.printf("과장: %s\n", name3);
+	System.out.printf("부장: %s\n\n", name4);
     }
 
 
