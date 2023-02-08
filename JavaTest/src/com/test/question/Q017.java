ed6a8fe7826639ad6bba768537d56650300d2137
hyowonhan <wain1719@gmail.com>
2023-02-08 11:47:59 +0900

    feat: solve Question 17

---
+++ JavaTest/src/com/test/question/Q017.java
@@ -0,0 +1,46 @@
+package com.test.question;
+
+public class Q017 {
+
+    /*
+
+    요구사항] 인자로 받은 이름들을 직급 순으로 출력하는 메소드를 선언하시오.
+
+    - 사원 → 대리 → 과장 → 부장
+    - void position(String)
+    - void position(String, String)
+    - void position(String, String, String)
+    - void position(String, String, String, String)
+
+    1. 메소드 생성
+    2. 메소드 호출(출력)
+
+    */
+
+    public static void main(String[] args) {
+
+	position("홍길");
+	position("홍길동", "유재석");
+	position("홍길동", "유재석", "박명수");
+	position("홍길동", "유재석", "박명수", "정형돈");
+
+    } //main
+
+    private static void position(String name) {
+	System.out.printf("사원: %s\n\n", name);
+    }
+
+    private static void position(String name1, String name2) {
+	System.out.printf("사원: %s\n대리: %s\n\n", name1, name2);
+    }
+
+    private static void position(String name1, String name2, String name3) {
+	System.out.printf("사원: %s\n대리: %s\n과장: %s\n\n", name1, name2, name3 );
+    }
+
+    private static void position(String name1, String name2, String name3, String name4) {
+	System.out.printf("사원: %s\n대리: %s\n과장: %s\n대리: %s\n\n", name1, name2, name3, name4);
+    }
+
+
+}
