f5802ff6b0b041bd68609bd7bf9eee0611815f4f
hyowonhan <wain1719@gmail.com>
2023-02-09 22:28:33 +0900

    feat: close scanner

--- JavaTest/src/com/test/question/Q031.java
+++ JavaTest/src/com/test/question/Q031.java
@@ -40,7 +40,9 @@ public class Q031 {
 	
 	System.out.printf("짝수는 %d개 홀수는 %d개 입력했습니다.\n",  even, odd);
 	System.out.printf("홀수가 짝수보다 %d개 더 많습니다.\n",  odd - even);
-
+	
+	scan.close();
+	
     } //main
     
     
