c7926f3c4d9afcff1696606ad941d345e5b7971e
hyowonhan <wain1719@gmail.com>
2023-02-14 16:34:30 +0900

    feat: solve question 70

---
+++ JavaTest/src/com/test/question/Q070.java
@@ -0,0 +1,58 @@
+package com.test.question;
+
+/*
+
+요구사항]배열의 요소를 삭제하시오.
+
+- 아래와 같이 출력하시오.
+
+- 홀수행 : 왼 -> 오
+- 짝수행 : 오 -> 왼
+
+ */
+
+public class Q070 {
+
+    public static void main(String[] args) {
+
+	int[][] list = new int[5][5];
+	int num = 1;
+
+	for(int i = 0; i < list.length; i++) {
+
+	    if(i % 2 == 0) {
+		for(int j = 0; j < list[0].length; j++) {
+
+		    list[i][j] = num;
+		    num++;
+
+		}
+	    } else {
+		for(int j = list[0].length - 1; j >= 0; j--) {
+
+		    list[i][j] = num;
+		    num++;
+		}
+	    }
+
+
+	}
+
+	 printArr(list);
+
+    } //main
+
+    private static void printArr(int[][] list) {
+
+	for(int i = 0; i < list.length; i++) {
+	    for(int j = 0; j < list[0].length; j++) {
+
+		System.out.printf("%2d\t", list[i][j]);
+	    }
+
+	    System.out.println();
+	}
+
+    } //printArr
+
+}
