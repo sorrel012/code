42c8765a77d028d6a0ffb74b875e8a29de93c74d
hyowonhan <wain1719@gmail.com>
2023-02-15 15:02:22 +0900

    feat : test code

--- codeReview/src/codeReview/Test.java
+++ codeReview/src/codeReview/Test.java
@@ -1,23 +1,52 @@
 package codeReview;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Test {
 
     public static void main(String[] args) {
-	int num = 0;
 
-	Scanner scan = new Scanner(System.in);
+	/*Q066.java
 
-	System.out.print("행: ");
-	num = scan.nextInt();
+	[요구사항] 배열: 로또
+	중복되지 않는 임의의 숫자 6개를 만드시오.(로또)
 
-	for(int i=0; i<num; ++i) {
-		for(int j=0; j<=i; j++) {
-			System.out.print("*");
+	조건
+	 - 숫자의 범위: 1 ~ 45
+	 - 오름차순 정렬
+	*/
+
+	int[] nums = new int[6];
+
+	for (int i = 0; i < nums.length; i++) {
+		nums[i] = (int)(Math.random() * 45) + 1;
+		//System.out.printf("%d, ", nums[i]);
+
+		for (int j = 0; j < i; j++) {
+			if (nums[i] == nums[j]) {
+				i--;
+			}
 		}
-		System.out.println();
 	}
-	scan.close();
-    }
+
+	Arrays.sort(nums);
+
+	String txt = "[";
+
+	for (int i = 0; i < nums.length; i++) {
+		txt += nums[i];
+
+		if (i == nums.length - 1) {
+			break;
+		}
+
+		txt += ", ";
+	}
+
+	String txt2 = "]";
+
+	System.out.print(txt + txt2);
+}
+
+
 }
