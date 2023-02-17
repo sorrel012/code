8a6c062f670b1e3b59936599fb24096ebe53fbd7
hyowonhan <wain1719@gmail.com>
2023-02-17 11:21:53 +0900

    feat: convey Instance to class

---
+++ JavaTest/src/com/test/obj/Ex45_Class.java
@@ -0,0 +1,22 @@
+package com.test.obj;
+
+public class Ex45_Class {
+
+	public static void main(String[] args) {
+
+		Employee e1 = new Employee();
+
+		e1.setName("홍길동");
+		e1.setDepartment("영업부");
+		e1.setPosition("과장");
+
+		Employee e2 = new Employee();
+
+		e2.setName("아무개");
+		e2.setDepartment("영업부");
+		e2.setPosition("대리");
+
+		e2.setSuperior(e1); //e1 객체를 통채로 superior로 전달
+
+	} //main
+}
