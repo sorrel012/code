0218d06b358d6c11f262e617b9514a5f7bdd6753
hyowonhan <wain1719@gmail.com>
2023-02-20 10:38:30 +0900

    feat: review constructor

---
+++ JavaTest/src/com/test/obj/inheritance/Mouse.java
@@ -0,0 +1,37 @@
+package com.test.obj.inheritance;
+
+public class Mouse {
+	
+	private int a;
+	public int b;
+	
+	public Mouse() {
+		this(0,0);
+	}
+	
+	public Mouse(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+	
+	public String info() {
+		return this.a + ", " + this.b;
+	}
+
+}
