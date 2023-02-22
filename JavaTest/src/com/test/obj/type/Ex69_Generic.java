042129ea8b7ca91c611ac19b0c6dfd24a6134e99
hyowonhan <wain1719@gmail.com>
2023-02-22 12:42:26 +0900

    feat: use generic class

--- JavaTest/src/com/test/obj/type/Ex69_Generic.java
+++ JavaTest/src/com/test/obj/type/Ex69_Generic.java
@@ -4,50 +4,84 @@ public class Ex69_Generic {
 
 	public static void main(String[] args) {
 
+//		m1();
+		m2();
+		
+	} //main
+
+	private static void m2() {
+		
+		
+		
+	}
+
+	private static void m1() {
+
 		//요구사항] 클래스 설계
-		
+
 		WrapperInt n1 = new WrapperInt(10);
-		
+
 		System.out.println(n1.getData());
 		System.out.println(n1.getData() * 2);
 		System.out.println(n1.toString());
 		System.out.println();
-		
 
 		WrapperObject n2 = new WrapperObject(20);	//업캐스팅
 		System.out.println(n2.getData());
 		System.out.println((int)n2.getData() * 2);	//다운캐스팅
 		System.out.println(n2.toString());
 		System.out.println();
-		
-		
-		WrapperString s1 = new WrapperString("홍길동");
-		
+
+		Wrapper<Integer> n3 = new Wrapper<Integer>(30);
+		System.out.println(n3.getData());
+		System.out.println(n3.getData() * 2);
+		System.out.println(n3.toString());
+		System.out.println();
+		System.out.println();
+
+
+		WrapperString s1 = new WrapperString("나무");
+
 		System.out.println(s1.getData());
 		System.out.println(s1.getData().length());
 		System.out.println(s1.toString());
 		System.out.println();
-		
+
 		WrapperObject s2 = new WrapperObject("홍길동");			//업캐스팅
 		System.out.println(s2.getData());
 		System.out.println(((String)s2.getData()).length());	//다운캐스팅
 		System.out.println(s2.toString());
 		System.out.println();
-		
+
+		Wrapper<String> s3 = new Wrapper<String>("이클립스");
+		System.out.println(s3.getData());
+		System.out.println(s3.getData().length());
+		System.out.println(s3.getData().toString());
+		System.out.println();
+		System.out.println();
+
 
 		WrapperBoolean b1 = new WrapperBoolean(true);
 		System.out.println(b1.getData());
 		System.out.println(b1.getData() ? "참" : "거짓");
 		System.out.println(b1.toString());
 		System.out.println();
-		
-		WrapperObject b2 = new WrapperObject(true);				//업캐스팅
+
+		WrapperObject b2 = new WrapperObject(false);				//업캐스팅
 		System.out.println(b2.getData());
 		System.out.println((boolean)b2.getData() ? "참" : "거짓");	//다운캐스팅
 		System.out.println(b2.toString());
 		System.out.println();
+
+		Wrapper<Boolean> b3 = new Wrapper<Boolean>(true);
+		System.out.println(b3.getData());
+		System.out.println(b3.getData() ? "참" : "거짓");
+		System.out.println(b3.getData().toString());
+		System.out.println();
+		System.out.println();
+		System.out.println();
 		
-	} //main
+	}
 	
 } //Main
 
@@ -149,3 +183,27 @@ class WrapperObject {
 	}
 		
 }
+
+class Wrapper<T> {
+	
+	private T data;
+
+	public Wrapper(T data) {
+		this.setData(data);
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "data=" + data;
+	}
+	
+}
+
