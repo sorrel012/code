4950bde6d2a1103860f8f86557001372a8114b84
hyowonhan <wain1719@gmail.com>
2023-02-22 11:47:02 +0900

    feat: use Object class

--- JavaTest/src/com/test/obj/type/Ex69_Generic.java
+++ JavaTest/src/com/test/obj/type/Ex69_Generic.java
@@ -11,20 +11,41 @@ public class Ex69_Generic {
 		System.out.println(n1.getData());
 		System.out.println(n1.getData() * 2);
 		System.out.println(n1.toString());
+		System.out.println();
+		
 
+		WrapperObject n2 = new WrapperObject(20);	//업캐스팅
+		System.out.println(n2.getData());
+		System.out.println((int)n2.getData() * 2);	//다운캐스팅
+		System.out.println(n2.toString());
+		System.out.println();
+		
 		
 		WrapperString s1 = new WrapperString("홍길동");
 		
 		System.out.println(s1.getData());
 		System.out.println(s1.getData().length());
 		System.out.println(s1.toString());
+		System.out.println();
+		
+		WrapperObject s2 = new WrapperObject("홍길동");			//업캐스팅
+		System.out.println(s2.getData());
+		System.out.println(((String)s2.getData()).length());	//다운캐스팅
+		System.out.println(s2.toString());
+		System.out.println();
 		
 
 		WrapperBoolean b1 = new WrapperBoolean(true);
-		
 		System.out.println(b1.getData());
 		System.out.println(b1.getData() ? "참" : "거짓");
 		System.out.println(b1.toString());
+		System.out.println();
+		
+		WrapperObject b2 = new WrapperObject(true);				//업캐스팅
+		System.out.println(b2.getData());
+		System.out.println((boolean)b2.getData() ? "참" : "거짓");	//다운캐스팅
+		System.out.println(b2.toString());
+		System.out.println();
 		
 	} //main
 	
@@ -101,6 +122,30 @@ class WrapperBoolean {
 	@Override
 	public String toString() {
 		return "date = " + this.data;
+	}
+		
+}
+
+class WrapperObject {
+	
+	private Object data;
+
+	public WrapperObject(Object data) {
+		super();
+		this.data = data;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "[data=" + data + "]";
 	}
 		
 }
