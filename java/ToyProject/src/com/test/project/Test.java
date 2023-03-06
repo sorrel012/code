e6e9a0da0da19c6880661f5532f337b998932094
hwh <sorrel012@gmail.com>
2023-03-06 11:50:40 +0900

    feat: apply font

---
+++ java/ToyProject/src/com/test/project/Test.java
@@ -0,0 +1,17 @@
+package com.test.project;
+
+public class Test {
+	
+	public static void main(String[] args) {
+
+		try {
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	} //main
+
+}
