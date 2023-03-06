fe78a96e76c51bd96d9553272ca980b31d892239
hwh <sorrel012@gmail.com>
2023-03-06 11:54:21 +0900

    feat: apply font

--- java/ToyProject/src/com/test/project/Test.java
+++ java/ToyProject/src/com/test/project/Test.java
@@ -1,11 +1,15 @@
 package com.test.project;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 public class Test {
 	
 	public static void main(String[] args) {
 
 		try {
-			
+
+			String asciiArt3 = FigletFont.convertOneLine(".\\font\\banner3.flf", "hello");
+		    System.out.println(asciiArt3);
 			
 			
 		} catch (Exception e) {
