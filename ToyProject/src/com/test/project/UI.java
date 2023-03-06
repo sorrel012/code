c94b7de53301a60b9a0db1029cbcdc0d1a6bb940
hwh <sorrel012@gmail.com>
2023-03-06 10:28:57 +0900

    feat: create new project

---
+++ ToyProject/src/com/test/project/UI.java
@@ -0,0 +1,17 @@
+package com.test.project;
+
+public class UI {
+
+	public static void mainMenu() {
+		
+		System.out.println("========================");
+		System.out.println("[회원 관리 프로그램]");
+		System.out.println("1. 목록 보기");
+		System.out.println("2. 추가하기");
+		System.out.println("3. 삭제하기");
+		System.out.println("4. 종료");
+		System.out.println("========================");
+		
+	}
+	
+}
