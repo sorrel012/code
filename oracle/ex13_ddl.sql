158495b50e794947b9615255685548c98c878330
hwh <sorrel012@gmail.com>
2023-03-16 17:38:36 +0900

    feat: study constraint

--- oracle/ex13_ddl.sql
+++ oracle/ex13_ddl.sql
@@ -35,4 +35,140 @@ select * from tblMemo;
 
 
 
+drop table tblMemo;
 
+/* not null*/
+
+--메모 테이블
+create table tblMemo (
+
+    --컬럼명 자료형(길이) NULL 제약사항
+    seq number(3) not null,         --메모번호
+    name varchar2(30) null,         --작성자
+    memo varchar2(1000) not null,   --메모
+    regdate date null               --작성날짜
+
+);
+
+
+insert into tblMemo (seq, name, memo, regdate)
+            values (1, '홍길동', '메모입니다.', sysdate);
+
+insert into tblMemo (seq, name, memo, regdate)
+            values (2, '홍길동', null, sysdate); -- not null 제약조건 때문에 에러!
+            
+insert into tblMemo (seq, name, memo, regdate)
+            values (3, '홍길동', '', sysdate); -- not null 제약조건 때문에 에러!
+
+select * from tblMemo;
+
+drop table tblMemo;
+
+
+/* primary key */
+
+--메모 테이블
+create table tblMemo (
+
+    --컬럼명 자료형(길이) NULL 제약사항
+    seq number(3) primary key,       --메모번호(일련번호)
+    name varchar2(30) null,         --작성자
+    memo varchar2(1000) not null,   --메모
+    regdate date null               --작성날짜
+
+);
+
+insert into tblMemo (seq, name, memo, regdate)
+            values (1, '홍길동', '메모입니다.', sysdate);
+
+insert into tblMemo (seq, name, memo, regdate)
+            values (1, '아무개', '메모입니당.', sysdate); --unique 제약조건 위반
+
+insert into tblMemo (seq, name, memo, regdate)
+            values (2, '아무개', '메모입니당.', sysdate);
+            
+insert into tblMemo (seq, name, memo, regdate)
+            values (null, '아무개', '메모입니당.', sysdate); --null 불가
+            
+select * from tblMemo;
+
+
+/* unique */
+
+create table tblMemo (
+
+    --컬럼명 자료형(길이) NULL 제약사항
+    seq number(3) primary key,      --메모번호(일련번호)
+    name varchar2(30) unique,       --작성자 > 한 사람이 딱 한 개의 글 작성 가능
+    memo varchar2(1000) not null,   --메모
+    regdate date null               --작성날짜
+
+);
+
+insert into tblMemo (seq, name, memo, regdate)
+            values (1, '홍길동', '메모입니다.', sysdate);
+
+insert into tblMemo (seq, name, memo, regdate)
+            values (2, '홍길동', '메모입니당~~', sysdate);  --unique 제약 위배
+
+insert into tblMemo (seq, name, memo, regdate)
+            values (2, '아무개', '메모입니당~~', sysdate);  
+            
+insert into tblMemo (seq, name, memo, regdate)
+            values (3, null, '메모!', sysdate);  
+
+/* check */
+
+drop table tblMemo;
+
+create table tblMemo (
+
+    --컬럼명 자료형(길이) NULL 제약사항
+    seq number(3) primary key,                                          --메모번호(일련번호)
+    name varchar2(30) check(length(name) > 1),                          --작성자 
+    memo varchar2(1000),                                                --메모
+    regdate date null,                                                  --작성날짜
+    priority number check(priority between 1 and 3),                    --1(중요), 2(보통), 3(사소)
+    category varchar2(30) check(category in ('할일', '장보기', '공부'))
+    
+);
+       
+insert into tblMemo (seq, name, memo, regdate, priority, category)
+            values (1, '홍길동', '메모입니다.', sysdate, 1, '할일'); 
+
+insert into tblMemo (seq, name, memo, regdate, priority, category)
+            values (2, '홍', '메모입니다.', sysdate, 10, '공부');  --check 제약조건 위배           
+            
+select * from tblMemo;
+
+
+/* default */
+
+drop table tblMemo;
+
+create table tblMemo (
+
+    --컬럼명 자료형(길이) NULL 제약사항
+    seq number(3) primary key,        --메모번호(일련번호)
+    name varchar2(30) default '익명', --작성자 > 한 사람이 딱 한 개의 글 작성 가능
+    memo varchar2(1000),              --메모
+    regdate date default sysdate      --작성날짜
+    
+);
+
+insert into tblMemo (seq, name, memo, regdate)
+            values (1, '홍길동', '메모입니다.', sysdate);
+
+insert into tblMemo (seq, name, memo, regdate)
+            values (2, '홍길동', '메모입니다~~', '2023-01-01');
+            
+insert into tblMemo (seq, name, memo, regdate)
+            values (3, null, '메모입니다.', sysdate);
+
+insert into tblMemo (seq, memo, regdate)
+            values (4, '메모입니당!!', sysdate);
+
+insert into tblMemo (seq, memo, regdate)
+            values (5, '메모 남기는 중', default);
+            
+select * from tblMemo;
