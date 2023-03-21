34e8020cad543c4d0b547c350cecfe419dcaa165
hwh <sorrel012@gmail.com>
2023-03-21 23:04:14 +0900

    refactor: correct the codes to make more efficient

--- oracle/ex19_join.sql
+++ oracle/ex19_join.sql
@@ -618,7 +618,8 @@ select
 from tblStaff s
     left outer join tblProject p
         on s.seq = p.staff_seq
-            order by s.name asc;       
+            order by s.name asc;      
+            
        
        
 -- tblVideo, tblRent, tblMember. '뽀뽀할까요' 라는 비디오를 빌려간 회원의 이름은?
@@ -626,12 +627,24 @@ select * from tblVideo;
 select * from tblMember;
 select * from tblRent;
 
+--inner join + subquery
 select
     m.name
 from tblMember m
     inner join tblRent r
         on m.seq = r.member
             where r.video = (select seq from tblVideo where name = '뽀뽀할까요') ;
+
+-- inner join + inner join
+select
+    m.name
+from tblMember m
+    inner join tblRent r
+        on m.seq = r.member
+            inner join tblVideo v
+                on r.video = v.seq
+    where v.name = '뽀뽀할까요';
+    
     
     
 -- tblStaff, tblProject. 'TV 광고'을 담당한 직원의 월급은 얼마인가?
@@ -644,13 +657,15 @@ from tblStaff s
     inner join tblProject p
         on s.seq = p.staff_seq
             where p.project = 'TV 광고';
-    
-    
+            
+            
+            
 -- tblVideo, tblRent, tblMember. '털미네이터' 비디오를 한번이라도 빌려갔던 회원들의 이름은?
 select * from tblVideo; -- v.name(털미네이터)의 비디오 번호(v.seq)
 select * from tblMember; -- r.member에 해당하는 회원의 이름 (m.name)
 select * from tblRent; -- v. seq를 한 번이라도 빌려간 적 있는(r.video) 회원 번호(r.member)
 
+--inner join + subquery
 select 
     m.name
 from tblMember m
@@ -658,6 +673,15 @@ from tblMember m
         on m.seq = r.member
             where r.video = (select v.seq from tblVideo v
                                 where v.name = '털미네이터');
+
+-- inner join + inner join
+SELECT DISTINCT m.name FROM tblmember m
+    inner join tblrent r
+        on m.seq = r.member
+            inner join tblvideo v
+                on r.video = v.seq
+    where v.name = '털미네이터' and r.rentdate is not null;
+
 
                 
 -- tblStaff, tblProject. 서울시에 사는 직원을 제외한 나머지 직원들의 이름, 월급, 담당프로젝트명을 가져오시오.
@@ -671,7 +695,8 @@ select
 from tblStaff s
     left outer join tblProject p
         on s.seq = p.staff_seq
-            where s.address <> '서울시';
+where s.address <> '서울시';
+
 
         
 -- tblCustomer, tblSales. 상품을 2개(단일상품) 이상 구매한 회원의 연락처, 이름, 구매상품명, 수량을 가져오시오.
@@ -679,22 +704,22 @@ select * from tblCustomer; -- seq /tel, name
 select * from tblSales; -- cseq / item, qty
 
 select
-    c.tel,
     c.name,
+    c.tel,
     s.item,
     s.qty
 from tblCustomer c
     inner join tblSales s
         on c.seq = s.cseq
-            where qty >= 2
+            where s.qty >= 2
                 order by name asc, qty desc;
 
+                                
                                 
 -- tblVideo, tblGenre. 모든 비디오 제목, 보유수량, 대여가격을 가져오시오.
 select * from tblGenre; -- seq / price
 select * from tblVideo; -- genre / name / qty
 
---TODO 시간 되면 tblRent에서 빌려간 수량 빼서 계산해보기..
 select 
     v.name,
     v.qty,
@@ -702,6 +727,8 @@ select
 from tblGenre g
     inner join tblVideo v
         on g.seq = v.genre
+    ORDER BY v.qty desc;            
+          
           
                 
 -- tblVideo, tblRent, tblMember, tblGenre. 2022년 2월에 대여된 구매내역을 가져오시오. 회원명, 비디오명, 언제, 대여가격
@@ -711,7 +738,7 @@ select * from tblMember; --seq / name
 select * from tblRent;  --member/video / rentdate
 
 select
-    (select name from tblMember m where m.seq = r.member) as member,
+    m.name as member,
     v.name,
     r.rentdate,
     g.price
@@ -720,7 +747,10 @@ from tblGenre g
         on g.seq = v.genre
             inner join tblRent r
                 on v.seq = r.video
-                    where substr(r.rentdate, 4,2) = '02';
+                    inner join tblMember m
+                        on m.seq = r.member
+where substr(r.rentdate, 4,2) = '02';
+  
         
         
 -- tblVideo, tblRent, tblMember. 현재 반납을 안한 회원명과 비디오명, 대여날짜를 가져오시오.
@@ -740,6 +770,7 @@ from tblMember m
 where r.retdate is null;
     
     
+    
 -- employees, departments. 사원들의 이름, 부서번호, 부서명을 가져오시오.
 select * from employees; -- department_id / first_name || ' ' || last_name, department_id
 select * from departments; -- department_id/department_name
@@ -751,6 +782,7 @@ select
 from departments d
     inner join employees e
         on d.department_id = e.department_id;    
+        
         
         
 -- employees, jobs. 사원들의 정보와 직업명을 가져오시오.
@@ -765,16 +797,30 @@ from jobs j
         on j.job_id = e.job_id;
         
         
+       
 -- employees, jobs. 직무(job_id)별 최고급여(max_salary) 받는 사원 정보를 가져오시오.
 select * from employees; --job_id
 select * from jobs; --job_id / max_salary
 
---TODO????????????????????????
-select 
-    *
-from jobs j
-    inner join employees e
-        on j.job_id = e.job_id
+--employees만 사용 > 현재 직원들의 급여중 최고급여
+SELECT 
+    e.*
+FROM employees e
+    INNER JOIN (
+      SELECT job_id, MAX(salary) AS max_salary
+      FROM employees
+        GROUP BY job_id
+    ) e2 ON e.job_id = e2.job_id AND e.salary = e2.max_salary
+    
+    
+--jobs의 max_salary를 받는 사원 정보
+SELECT 
+    e.* 
+FROM employees e
+    INNER JOIN jobs j
+        on e.job_id = j.job_id
+WHERE e.salary = j.max_salary;
+
 
     
 -- departments, locations. 모든 부서와 각 부서가 위치하고 있는 도시의 이름을 가져오시오.
@@ -789,6 +835,7 @@ from locations l
         on l.location_id = d.location_id;
         
         
+        
 -- locations, countries. location_id 가 2900인 도시가 속한 국가 이름을 가져오시오.
 select * from locations;
 select * from countries;
@@ -799,6 +846,7 @@ from countries c
     inner join locations l
         on l.country_id = c.country_id
             where l.location_id = 2900;
+            
             
             
 -- employees. 급여를 12000 이상 받는 사원과 같은 부서에서 근무하는 사원들의 이름, 급여, 부서번호를 가져오시오.
@@ -812,7 +860,16 @@ select
 from employees
     where department_id in (select department_id from employees where salary >= 12000)
         order by department_id asc;
-            
+
+-- TODO 왜 job_id?            
+SELECT 
+    e.first_name||' '||e.last_name as name, 
+    e.salary, 
+    e.department_id 
+FROM HR.employees e
+    inner join (SELECT DISTINCT (job_id) FROM HR.employees where salary >= 12000) f
+        on e.job_id = f.job_id
+        order by department_id asc;
         
 -- employees, departments. locations.  'Seattle'에서(LOC) 근무하는 사원의 이름, job_id, 부서번호, 부서이름을 가져오시오.
 select * from employees; -- (first_name || ' ' || last_name) as name, department_id
@@ -830,18 +887,21 @@ from employees e
             where d.location_id = (select location_id from locations 
                                     where city = 'Seattle');    
 
-    
+
+
 -- employees, departments. first_name이 'Jonathon'인 직원과 같은 부서에 근무하는 직원들 정보를 가져오시오.
 select * from employees; -- first name, department_id
 select * from departments; --사용xx
 
+--TODO 본인 제외?
 select
     *
 from employees
     where department_id = (select department_id from employees 
                             where first_name = 'Jonathon')
     and first_name <> 'Jonathon';
-    
+   
+   
     
 -- employees, departments. 사원이름과 그 사원이 속한 부서의 부서명, 그리고 월급을 출력하는데 월급이 3000이상인 사원을 가져오시오.
 select * from employees; -- (first_name || ' ' || last_name) as name, department_id, salary
@@ -849,14 +909,16 @@ select * from departments; --department_id, department_name
 
 select 
     (e.first_name || ' ' || e.last_name) as name,
-    d.department_name
+    d.department_name,
+    e.salary
 from departments d
     inner join employees e
         on d.department_id = e.department_id
 where e.salary >= 3000
     order by e.salary desc;
     
-            
+    
+           
 -- employees, departments. 부서번호가 10번인 사원들?의 부서번호, 부서이름, 사원이름, 월급을 가져오시오.
 select * from employees; -- department_id, (first_name || ' ' || last_name) as name, salary
 select * from departments; --department_id, department_name
@@ -872,6 +934,7 @@ from departments d
 where e.department_id = 10;
             
             
+           
 -- departments, job_history. 퇴사한 사원의 입사일, 퇴사일, 근무했던 부서 이름을 가져오시오.
 select * from departments; -- department_id/ department_name
 select * from job_history; -- department_id/start_date,end_date
@@ -883,6 +946,7 @@ select
 from departments d
     inner join job_history h
         on d.department_id = h.department_id;
+        
         
         
 -- employees. 사원번호와 사원이름, 그리고 그 사원을 관리하는 관리자의 사원번호와 사원이름을 출력하되 각각의 컬럼명을 '사원번호', '사원이름', '관리자번호', '관리자이름'으로 하여 가져오시오.
@@ -899,18 +963,20 @@ from employees e1
 order by e1.employee_id;
 
 
+
 -- employees, jobs. 직책(Job Title)이 Sales Manager인 사원들의 입사년도와 입사년도(hire_date)별 평균 급여를 가져오시오. 년도를 기준으로 오름차순 정렬.
 select * from employees; -- job_id/ hire_date, salary, 
 select * from jobs; --job_id / job_title='Sales Manager' / 
 
 select 
-    substr(e.hire_date,1,2) as hire_year,
+    to_char(e.hire_date, 'yyyy') || '년도' as "입사년도",
     avg(e.salary)
 from employees e
     where job_id = (select job_id from jobs where job_title = 'Sales Manager')
-        group by substr(e.hire_date,1,2)
-            order by substr(e.hire_date,1,2) asc;
-
+        group by to_char(e.hire_date, 'yyyy')
+            order by to_char(e.hire_date, 'yyyy') asc;
+            
+            
 
 -- employees, departments. locations. 각 도시(city)에 있는 모든 부서 사원들의 평균급여가 가장 낮은 도시부터 도시명(city)과 평균연봉, 해당 도시의 사원수를 가져오시오.
 -- 단, 도시에 근 무하는 사원이 10명 이상인 곳은 제외하고 가져오시오.
@@ -927,8 +993,10 @@ from employees e
         on d.department_id = e.department_id
             inner join locations l
                 on d.location_id = l.location_id
-group by l.city
-    order by avg(e.salary) asc;
+    group by l.city
+        having count(*) < 10
+        order by avg(e.salary) asc;
+            
             
             
 -- employees, jobs, job_history. ‘Public Accountant’의 직책(job_title)으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 가져오시오. 
@@ -938,14 +1006,15 @@ select * from jobs; --job_id
 select * from job_history; -- employee_id, job_id
 
 select
-    h.employee_id,
-    (e.first_name || ' ' || e.last_name) as name
+    h.employee_id as "사번",
+    (e.first_name || ' ' || e.last_name) as "이름"
 from employees e
     inner join job_history h
         on e.employee_id = h.employee_id
 where h.job_id = (select job_id from jobs where job_title = 'Public Accountant');
     
     
+
 -- employees, departments, locations. 커미션을 받는 모든 사람들의 first_name, last_name, 부서명, 지역 id, 도시명을 가져오시오.
 select * from employees; -- department_id/first_name, last_name + where commission_pct is not null
 select * from departments; -- department_id / location_id/ department_name
@@ -962,7 +1031,9 @@ from employees e
             left outer join locations l
                 on l.location_id = d.location_id
 where e.commission_pct is not null;
-        
+ 
+
+       
 -- employees. 자신의 매니저보다 먼저 고용된 사원들의 first_name, last_name, 고용일을 가져오시오.
 select * from employees; 
 
@@ -974,5 +1045,4 @@ from employees e1
     inner join employees e2
         on e1.manager_id = e2.employee_id
 where e1.hire_date < e2.hire_date;
-            
-            
+
