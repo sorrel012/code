de41c2981ca1906039fa58c25200102dd37e2fe1
hwh <sorrel012@gmail.com>
2023-03-22 17:20:06 +0900

    feat: solve questions

--- oracle/ex23_qseudo.sql
+++ oracle/ex23_qseudo.sql
@@ -73,4 +73,89 @@ SELECT a.*, ROWNUM AS rnum FROM(SELECT * FROM tblcomedian ORDER BY weight DESC) 
 SELECT * FROM (SELECT a.*, ROWNUM AS rnum FROM(SELECT * FROM tblcomedian ORDER BY weight DESC) a)
     WHERE rnum = 5;
     
+
+--====================================================Q=======================================================
+
+-- 1. tblInsa. 남자 급여(기본급+수당)을 (내림차순)순위대로 가져오시오. (이름, 부서, 직위, 급여, 순위 출력)
+SELECT name, buseo, jikwi, basicpay+sudang, rnum FROM(SELECT a.*, ROWNUM AS rnum FROM(SELECT * FROM tblinsa 
+                                                        WHERE substr(ssn, 8, 1) = '1' 
+                                                           ORDER BY basicpay+sudang DESC) a);
+
+
+-- 2. tblInsa. 여자 급여(기본급+수당)을 (오름차순)순위대로 가져오시오. (이름, 부서, 직위, 급여, 순위 출력)
+SELECT name, buseo, jikwi, basicpay+sudang, rnum FROM(SELECT a.*, ROWNUM AS rnum FROM(SELECT * FROM tblinsa 
+                                                          WHERE substr(ssn, 8, 1) = '2' 
+                                                             ORDER BY basicpay+sudang DESC) a);
+                                                                                
+-- 3. tblInsa. 여자 인원수가 (가장 많은 부서 및 인원수) 가져오시오.
+select * from (select a.*, rownum as rnum from(select buseo, count(*) as count  from tblInsa 
+                                where substr(ssn, 8, 1) = '2'
+                                    group by buseo
+                                        order by count(*) desc) a)
+    where rnum = 1;
+
+
+-- 4. tblInsa. 지역별 인원수 (내림차순)순위를 가져오시오.(city, 인원수)
+select a.*, rownum
+    from (select city, count(*) as count 
+            from tblInsa group by city order by count(*) desc) a;            
+
+
+-- 5. tblInsa. 부서별 인원수가 가장 많은 부서명 및 인원수 출력.
+select a.*, rownum
+    from (select buseo, count(*) as count from tblInsa 
+            group by buseo order by count(*) desc) a
+    where rownum = 1;
+
+
+-- 6. tblInsa. 남자 급여(기본급+수당)을 (내림차순) 3~5등까지 가져오시오. (이름, 부서, 직위, 급여, 순위 출력)
+SELECT name, buseo, jikwi, basicpay+sudang, rnum 
+    FROM(SELECT a.*, ROWNUM AS rnum FROM(SELECT * FROM tblinsa 
+                                            WHERE substr(ssn, 8, 1) = '1' 
+                                               ORDER BY basicpay+sudang DESC) a)
+where rnum between 3 and 5;
+
+
+-- 7. tblInsa. 입사일이 빠른 순서로 5순위까지만 가져오시오.
+select * from (select a.*, rownum from(select * from tblInsa order by ibsadate) a)
+    where rownum <= 5; 
+
+
+-- 8. tblhousekeeping. 지출 내역(가격 * 수량) 중 가장 많은 금액을 지출한 내역 3가지를 가져오시오.
+select a.*, rownum from (select * from tblhousekeeping
+                           order by price*qty desc) a
+    where rownum <= 3;
+    
+
+-- 9. tblinsa. 평균 급여 2위인 부서에 속한 직원들을 가져오시오.
+select * from tblInsa 
+    where buseo = (select buseo from (select a.*, rownum as rnum from (select round(avg(basicpay+sudang), 2) as "avg(salary)", buseo from tblinsa 
+                                                    group by buseo order by round(avg(basicpay+sudang), 2) desc)a)
+                    where rnum = 2);   
+
+
+-- 10. tbltodo. 등록 후 가장 빠르게 완료한 할일을 순서대로 5개 가져오시오.
+select a.*, rownum from (select * from tblTodo where completedate is not null 
+                            order by completedate-adddate) a
+    where rownum <= 5;
+
+
+-- 11. tblinsa. 남자 직원 중에서 급여를 3번째로 많이 받는 직원과 9번째로 많이 받는 직원의 급여 차액은 얼마인가?
+
+select 
+    (select basicpay+sudang from (select a.*, rownum as rnum from(select * from tblInsa 
+                                 where substr(ssn, 8, 1) = '1'
+                                      order by basicpay+sudang desc) a)
+        where rnum = 3)
+    -
+    (select basicpay+sudang from (select a.*, rownum as rnum from(select * from tblInsa 
+                                    where substr(ssn, 8, 1) = '1'
+                                        order by basicpay+sudang desc) a)
+        where rnum = 9) as gap        
+from dual;
+
+
+
+
+
     
