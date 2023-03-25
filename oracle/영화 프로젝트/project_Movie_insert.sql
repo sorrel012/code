e87717c480096df69a6082c258d14bd389140889
hwh <sorrel012@gmail.com>
2023-03-25 14:23:16 +0900

    feat: create insert statement of tblNation, tblDirector

---
+++ oracle/영화 프로젝트/project_Movie_insert.sql
@@ -0,0 +1,86 @@
+
+/*tblNation*/ --완료
+
+insert into tblNation values(seqNation.nextval, '일본');
+insert into tblNation values(seqNation.nextval, '한국');
+insert into tblNation values(seqNation.nextval, '미국');
+insert into tblNation values(seqNation.nextval, '영국');
+insert into tblNation values(seqNation.nextval, '아일랜드');
+insert into tblNation values(seqNation.nextval, '스웨덴');
+insert into tblNation values(seqNation.nextval, '이란');
+insert into tblNation values(seqNation.nextval, '프랑스');
+insert into tblNation values(seqNation.nextval, '독일');
+insert into tblNation values(seqNation.nextval, '인도');
+insert into tblNation values(seqNation.nextval, '뉴질랜드');
+insert into tblNation values(seqNation.nextval, '캐나다');
+
+
+/*tblGenre*/
+
+
+/*tblGrade*/
+
+
+/*tblActor*/
+
+
+/*tblDirector*/ --완료
+
+insert into tblDirector values(seqDirector.nextval,'신카이 마코토',to_date('1973-02-09', 'yyyy-mm-dd'),1,1);
+insert into tblDirector values(seqDirector.nextval,'웅남이',to_date('1981-08-15','yyyy-mm-dd'),2,2);
+insert into tblDirector values(seqDirector.nextval,'민용근',to_date('1976-08-02','yyyy-mm-dd'),5,2);
+insert into tblDirector values(seqDirector.nextval,'이노우에 다케히코',to_date('1967-01-12','yyyy-mm-dd'),null ,1);
+insert into tblDirector values(seqDirector.nextval,'데이비드F 샌드버그',to_date('1981-01-21','yyyy-mm-dd'),27 ,6);
+insert into tblDirector values(seqDirector.nextval,'스티븐 스필버그',to_date('1946-12-18','yyyy-mm-dd'),3,3);
+insert into tblDirector values(seqDirector.nextval,'소토자키 하루오',to_date('1973-12-29','yyyy-mm-dd'),null ,1);
+insert into tblDirector values(seqDirector.nextval,'이원태',to_date('1975-06-18','yyyy-mm-dd'),10,2);
+insert into tblDirector values(seqDirector.nextval,'다니엘 콴',to_date('1988-02-10','yyyy-mm-dd'),9 ,3);
+insert into tblDirector values(seqDirector.nextval,'다니엘 샤이너트',to_date('1987-06-07','yyyy-mm-dd'),null ,3);
+insert into tblDirector values(seqDirector.nextval,'장 프랑소와 리셰',to_date('1966-07-02','yyyy-mm-dd'),39 ,8);
+insert into tblDirector values(seqDirector.nextval,'김주환',to_date('1981-03-22','yyyy-mm-dd'), 34,2);
+insert into tblDirector values(seqDirector.nextval,'오윤동',to_date('1975-11-12','yyyy-mm-dd'), null,2);
+insert into tblDirector values(seqDirector.nextval,'정주리',to_date('1980-02-02','yyyy-mm-dd'), 31,2);
+insert into tblDirector values(seqDirector.nextval,'애나 릴리 아미푸르',to_date('1976-07-25','yyyy-mm-dd'),18,8);
+insert into tblDirector values(seqDirector.nextval,'조나단 골드스타인',to_date('1969-07-02','yyyy-mm-dd'),null ,3);
+insert into tblDirector values(seqDirector.nextval,'존 프란시스 데일리',to_date('1985-07-20','yyyy-mm-dd'), null,3);
+insert into tblDirector values(seqDirector.nextval,'M 나이트 샤말린',to_date('1970-08-06','yyyy-mm-dd'),8 ,10);
+insert into tblDirector values(seqDirector.nextval,'니콜라스D.존슨',to_date('1967-12-16','yyyy-mm-dd'), null,3);
+insert into tblDirector values(seqDirector.nextval,'윌 메릭',to_date('1993-04-09','yyyy-mm-dd'), null,4);
+insert into tblDirector values(seqDirector.nextval,'윤준형',to_date('1974-01-26','yyyy-mm-dd'),42 ,2);
+insert into tblDirector values(seqDirector.nextval,'마틴 맥도나',to_date('1970-03-26','yyyy-mm-dd'),4 ,4);
+insert into tblDirector values(seqDirector.nextval,'맷 러스킨',to_date('1981-03-26','yyyy-mm-dd'),43 ,3);
+insert into tblDirector values(seqDirector.nextval,'이병헌',to_date('1980-07-23','yyyy-mm-dd'),11 ,2);
+insert into tblDirector values(seqDirector.nextval,'정용기',to_date('1970-05-23','yyyy-mm-dd'), null,2);
+insert into tblDirector values(seqDirector.nextval,'토드 필드',to_date('1964-02-24','yyyy-mm-dd'), null,3);
+insert into tblDirector values(seqDirector.nextval,'페이튼 리드',to_date('1964-07-03','yyyy-mm-dd'), null,3);
+insert into tblDirector values(seqDirector.nextval,'권혁재',to_date('1980-05-13','yyyy-mm-dd'),null ,2);
+insert into tblDirector values(seqDirector.nextval,'장항준',to_date('1969-09-17','yyyy-mm-dd'),29 ,2);
+insert into tblDirector values(seqDirector.nextval,'마크 포스터',to_date('1969-11-30','yyyy-mm-dd'),35 ,9);
+insert into tblDirector values(seqDirector.nextval,'러셀 크로우',to_date('1964-04-07','yyyy-mm-dd'), 22,2);
+insert into tblDirector values(seqDirector.nextval,'형슬우',to_date('1987-02-11','yyyy-mm-dd'), null,2);
+insert into tblDirector values(seqDirector.nextval,'대런 아로노프스키',to_date('1969-02-12','yyyy-mm-dd'),28 ,3);
+insert into tblDirector values(seqDirector.nextval,'가이 리치',to_date('1968-09-10','yyyy-mm-dd'),null ,4);
+insert into tblDirector values(seqDirector.nextval,'아쉬가르 파라디',to_date('1972-01-01','yyyy-mm-dd'),36 ,10);
+insert into tblDirector values(seqDirector.nextval,'리스 플레이크 워터필드',to_date('1982-10-21','yyyy-mm-dd'),null ,3);
+insert into tblDirector values(seqDirector.nextval,'데이미언 셔젤',to_date('1985-01-19','yyyy-mm-dd'), null,3);
+insert into tblDirector values(seqDirector.nextval,'김수정',to_date('1950-07-31','yyyy-mm-dd'),null ,2);
+insert into tblDirector values(seqDirector.nextval,'하라 케이이치',to_date('1959-07-24','yyyy-mm-dd'), 43,1);
+insert into tblDirector values(seqDirector.nextval,'벤 애플렉',to_date('1968-08-15','yyyy-mm-dd'), null,3);
+insert into tblDirector values(seqDirector.nextval,'홍상수',to_date('1960-10-25','yyyy-mm-dd'),21 ,2);
+insert into tblDirector values(seqDirector.nextval,'채드 스타헬스키',to_date('1968-09-20','yyyy-mm-dd'),null,3);
+insert into tblDirector values(seqDirector.nextval,'에드워드 버거',to_date('7068-02-13','yyyy-mm-dd'), null,9);
+insert into tblDirector values(seqDirector.nextval,'이창열',to_date('1972-05-15','yyyy-mm-dd'), null,2);
+insert into tblDirector values(seqDirector.nextval,'안토인 르',to_date('1961-06-18','yyyy-mm-dd'), null,8);
+insert into tblDirector values(seqDirector.nextval,'제임스 카메론',to_date('1954-08-16','yyyy-mm-dd'), null,12);
+insert into tblDirector values(seqDirector.nextval,'임순례',to_date('1961-01-01','yyyy-mm-dd'), null,2);
+insert into tblDirector values(seqDirector.nextval,'제임스 건',to_date('1970-08-05','yyyy-mm-dd'), null,3);
+insert into tblDirector values(seqDirector.nextval,'제임스 맨골드',to_date('1963-12-16','yyyy-mm-dd'),null ,3);
+insert into tblDirector values(seqDirector.nextval,'이승원',to_date('1980-09-10','yyyy-mm-dd'), null,2);
+
+
+/*tblMovie*/
+
+/*tbScore*/
+
+
+/*tblViewer*/
