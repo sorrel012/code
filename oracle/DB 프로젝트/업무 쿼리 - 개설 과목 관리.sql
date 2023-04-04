59a593a52533183dcbd266983c3120abda4a134a
hwh <sorrel012@gmail.com>
2023-04-04 15:15:54 +0900

    feat: create code about subject

--- oracle/DB 프로젝트/업무 쿼리 - 개설 과목 관리.sql
+++ oracle/DB 프로젝트/업무 쿼리 - 개설 과목 관리.sql
@@ -11,9 +11,13 @@ A.04 개설 과목 관리
 
 /*개설 과목 정보*/
 
-
--- 1. 입력(과목명 / 과목 시작일, 과목 종료일, 과정번호, 과목번호, 교사번호 / 과목 번호, 교재번호)
-
+-- 1. 입력(과목 시작일, 과목 종료일, 과정번호, 과목번호, 교사번호 / 과목 번호, 교재번호)
+insert all
+    into tblCurSub(cursubStart, cursubEnd, curriculum_seq, subject_seq, teacher_seq)
+        values('2023-12-04', '2024-02-02', 37, 13, 11)
+    into tblSubBook(subject_seq, book_seq) 
+        values(13, 24)
+select * from dual;
 
 
 -- 2. 출력
@@ -44,20 +48,48 @@ where s.subject_seq = 1
     order by c.curriculumStart, cs.cursubStart;
 
 
--- 3-1. 수정
-create table tblBookSign(    
-    signature varchar2(20) null,
-    book_seq number not null,
-    student_seq number not null,
-
-    constraint bs_book_seq_fk foreign key(book_seq) references tblBook(book_seq),
-    constraint bs_student_seq_fk foreign key(student_seq) references tblStudent(student_seq)
-);
-
-select * from tblSubBook;
-
-
--- 4. 삭제
+-- 3-1. 과목 기간 수정
+update tblCurSub set cursubStart = '2023-12-04', cursubEnd = '2024-02-01'
+    where curriculum_seq = 37 and subject_seq = 13;
+    
+-- 3-2. 담당 교사 수정
+update tblCurSub set teacher_seq = 1
+    where curriculum_seq = 37 and subject_seq = 13;
+    
+-- 3-3. 교재 번호 수정
+update tblSubBook set book_seq = 25
+    where subject_seq = 13 and book_seq = 24;
 
 
-/* 특정 개설 과정 선택 시 개설 과목 정보 출력 및 개설 과목 신규 등록 */
+-- 4-1. 과목 정보 삭제
+delete tblCurSub 
+    where curriculum_seq = 37 and subject_seq = 13;
+
+-- 4-2. 과목별 교재 정보 삭제
+delete tblSubBook
+    where subject_seq = 13;
+    
+
+
+/* 특정 개설 과정 선택 시 개설 과목 정보 출력*/
+select distinct
+    c.curriculumName as "과정명",
+    s.subjectName as "과목명",
+    cs.cursubStart as "과목 시작일",
+    cs.cursubEnd as "과목 종료일",
+    b.bookName as "교재명",
+    t.teacherName as "교사명"
+from tblCurriculum c
+    inner join tblCursub cs
+        on c.curriculum_seq = cs.curriculum_seq 
+            inner join tblSubject s
+                on s.subject_seq = cs.subject_seq
+                    inner join tblSubBook sb
+                        on sb.subject_seq = s.subject_seq
+                            inner join tblTeacher t
+                                on t.teacher_seq = cs.teacher_seq
+                                    inner join tblBook b
+                                        on b.book_seq = sb.book_seq
+where c.curriculum_seq = 1
+    order by c.curriculumName, cs.cursubStart
+    
