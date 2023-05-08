125504854f32f551d1aab41d6c1e54cfdd453484
user <user@DESKTOP-COJ9858>
2023-05-08 14:03:39 +0900

    feat: Send and get data with Get method

---
+++ server/ServletTest/src/main/java/com/test/servlet/Ex04.java
@@ -0,0 +1,38 @@
+package com.test.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Ex04 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        resp.setCharacterEncoding("UTF-8");
+        
+        PrintWriter writer = resp.getWriter();
+        
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<meta charset='UTF-8'>");
+        writer.println("</head>");
+        writer.println("<h1>데이터 보내기</h1>");
+        writer.println("<form method='GET' action='/servlet/ex04ok'>");
+        writer.println("<div>이름: <input type='text' name='name'></div>");
+        writer.println("<div></div>");
+        writer.println("<div></div>");
+        writer.println("<div><input type='submit' value='보내기'></div>");
+        writer.println("</form>");
+        writer.println("</body>");
+        writer.println("</html>");
+        
+        writer.close();
+        
+    }
+    
+}
