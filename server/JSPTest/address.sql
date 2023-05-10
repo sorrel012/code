1938869569e22e6e8599a4fc1e89aa9f50304d82
hwh <sorrel012@gmail.com>
2023-05-10 16:32:57 +0900

    feat: Insert user data

--- server/JSPTest/address.sql
+++ server/JSPTest/address.sql
@@ -7,3 +7,20 @@ create table tblAddress (
 );
 
 create sequence seqAddress;
+
+select * from tblAddress;
+
+drop table tblUser cascade constraints;
+
+select * from tblUser;
+
+create table tblUser (
+    id varchar2(30) primary key,    --아이디(PK)
+    pw varchar2(30) not null,       --암호
+    name varchar2(30) not null,     --이름
+    lv number(1) not null           --등급(1-일반, 2-관리자)
+);
+
+insert into tblUser values('hong', '1111', '홍길동', 1);
+insert into tblUser values('test', '1111', '테스트', 1);
+insert into tblUser values('admin', '1111', '관리자', 2);
