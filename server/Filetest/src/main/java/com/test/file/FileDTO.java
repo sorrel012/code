2906afee643da16d790716f965d5f3794c308a22
sorrel012 <sorrel012@gmail.com>
2023-05-12 11:30:24 +0900

    feat: Implement Data Transfer Object

---
+++ server/Filetest/src/main/java/com/test/file/FileDTO.java
@@ -0,0 +1,42 @@
+package com.test.file;
+
+public class FileDTO {
+    
+    private String seq;
+    private String name;
+    private String filename;
+    private String regdate;
+    
+    public String getSeq() {
+        return seq;
+    }
+    
+    public void setSeq(String seq) {
+        this.seq = seq;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getFilename() {
+        return filename;
+    }
+    
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+    
+    public String getRegdate() {
+        return regdate;
+    }
+    
+    public void setRegdate(String regdate) {
+        this.regdate = regdate;
+    }
+    
+}
