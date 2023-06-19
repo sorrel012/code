02b688c4978f06e2e43835762153b9367ad367dc
sorrel012 <wain1719@gmail.com>
2023-06-19 11:55:34 +0900

    feat: Implement multifile upload

--- spring/FileTest/src/main/java/com/test/controller/FileController.java
+++ spring/FileTest/src/main/java/com/test/controller/FileController.java
@@ -23,30 +23,30 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FileController {
-	
+
 	@GetMapping("/add.do")
 	public String add() {
-		
+
 		return "add";
 	}
-	
+
 	@PostMapping("/addok.do")
 	public String addok(Model model, String txt, MultipartFile attach, HttpServletRequest req) {
-		
+
 		//System.out.println("txt: " + txt);
-		
+
 		System.out.println(attach.getName()); // <input type="file" name="attach"> 의 name>(태그명)
 		System.out.println(attach.getOriginalFilename()); // 업로드 파일
 		System.out.println(attach.getContentType()); // 파일 MIME
 		System.out.println(attach.getSize()); //사이즈(Byte)
 		System.out.println(attach.isEmpty()); //존재 유무
-		
+
 		//C:\OneDrive\class\code\spring\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\wtpwebapps\FileTest\resources\files
 		String path = req.getRealPath("/resources/files");
 		System.out.println(path);
-		
+
 		try {
-			
+
 			//파일명 중복 방지
 			//1. 숫자 붙이기
 			//2. 고유 파일명 만들기
@@ -55,63 +55,96 @@ public class FileController {
 			//3. UUID, Universally Uizue
 			//	- 네트워크 상에서 고유성이 보장되는 ID를 만들기 위한 표준
 			//	- 시간 + 난수 조합
-			
+
 			//System.out.println(System.nanoTime() + "_" + attach.getOriginalFilename());
-			
+
 			UUID uuid = UUID.randomUUID();
 			System.out.println(uuid);
-			
+
 			File file = new File(path + "\\" + uuid + "_" + attach.getOriginalFilename());
 			attach.transferTo(file);
-			
+
 			model.addAttribute("txt", txt);
 			model.addAttribute("filename", uuid + "_" + attach.getOriginalFilename()); // 하드디스크에 저장된 파일명
 			model.addAttribute("orgfilename", attach.getOriginalFilename()); // 사용자가 올린 파일명ㄴㄴㄴ
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+		return "addok";
+	}
+
+	@GetMapping(value = "/download.do", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent") String userAgent, String filename, HttpServletRequest req) {
+
+		String path = req.getRealPath("/resources/files");
+		Resource resource = new FileSystemResource(path + "\\" + filename);
+
+		if (resource.exists() == false) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		String resourceName = resource.getFilename();
+
+		// remove UUID
+		String resourceOriginalName = resourceName.substring(resourceName.indexOf("_") + 1);
+
+		HttpHeaders headers = new HttpHeaders();
+		try {
+
+			String downloadName = null;
+
+			if (userAgent.contains("Trident")) {
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8").replaceAll("\\+", " ");
+			} else if (userAgent.contains("Edge")) {
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8");
+			} else {
+				downloadName = new String(resourceOriginalName.getBytes("UTF-8"), "ISO-8859-1");
+			}
+
+
+			headers.add("Content-Disposition", "attachment; filename=" + downloadName);
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+	}
+
+	
+	@PostMapping("/multiaddok.do")
+	public String multiaddok(Model model, String txt, MultipartFile[] attach, HttpServletRequest req) {
+		
+		
+		for (MultipartFile file : attach) {
+			
+			System.out.println(file.getName()); // <input type="file" name="attach"> 의 name>(태그명)
+			System.out.println(file.getOriginalFilename()); // 업로드 파일
+			System.out.println(file.getContentType()); // 파일 MIME
+			System.out.println(file.getSize()); //사이즈(Byte)
+			System.out.println(file.isEmpty()); //존재 유무
+			System.out.println();
+			
+			try {
+				
+				UUID uuid = UUID.randomUUID();
+				
+				String filename = uuid + "_" + file.getOriginalFilename();
+				
+				file.transferTo(new File(req.getRealPath("/resources/files") + "\\" + filename));
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		model.addAttribute("txt", txt);
 		
 		return "addok";
 	}
-	
-	@GetMapping(value = "/download.do", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	   @ResponseBody
-	   public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent") String userAgent, String filename, HttpServletRequest req) {
-
-	      String path = req.getRealPath("/resources/files");
-	      Resource resource = new FileSystemResource(path + "\\" + filename);
-
-	      if (resource.exists() == false) {
-	         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	      }
-
-	      String resourceName = resource.getFilename();
-
-	      // remove UUID
-	      String resourceOriginalName = resourceName.substring(resourceName.indexOf("_") + 1);
-
-	      HttpHeaders headers = new HttpHeaders();
-	      try {
-
-	         String downloadName = null;
-
-	         if (userAgent.contains("Trident")) {
-	            downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8").replaceAll("\\+", " ");
-	         } else if (userAgent.contains("Edge")) {
-	            downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8");
-	         } else {
-	            downloadName = new String(resourceOriginalName.getBytes("UTF-8"), "ISO-8859-1");
-	         }
-
-
-	         headers.add("Content-Disposition", "attachment; filename=" + downloadName);
-
-	      } catch (UnsupportedEncodingException e) {
-	         e.printStackTrace();
-	      }
-
-	      return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
-	   }
 
 }
--- spring/FileTest/src/main/webapp/WEB-INF/views/add.jsp
+++ spring/FileTest/src/main/webapp/WEB-INF/views/add.jsp
@@ -9,13 +9,28 @@
 <link rel="stylesheet" href="https://me2.do/5BvBFJ57">
 <style>
 
+	#attach-zone {
+		border: 1px solid var(--border-color);
+		background-color: var(--back-color);
+		width: 300px;
+		height: 150px;
+		overflow: auto;
+	}
+	
+	#attach-zone .item {
+		display: flex;
+		justify-content: space-between;
+		font-size: 14px;
+		margin: 5px 10px;
+	}
+
 </style>
 </head>
 <body>
 
 	<h1>파일 업로드</h1>
 	
-	<form method="POST" action="/file/addok.do" enctype="multipart/form-data">
+	<form method="POST" action="/file/addok.do" enctype="multipart/form-data" id="form1">
 	<table class="vertical">
 		<tr>
 			<th>텍스트</th>
@@ -31,36 +46,80 @@
 	</div>
 	</form>
 	
+	
+	<hr>
+	
+	<h1>다중 파일 업로드(multiple)</h1>
+	
+	<form method="POST" action="/file/multiaddok.do" enctype="multipart/form-data" id="form2">
+	<table class="vertical">
+		<tr>
+			<th>텍스트</th>
+			<td><input type="text" name="txt" value="홍길동"></td>
+		</tr>
+		<tr>
+			<th>파일</th>
+			<td><input type="file" name="attach" multiple></td>
+		</tr>
+	</table>
+	<div>
+		<button>보내기</button>
+	</div>
+	</form>
+	
+	
+	<hr>
+	
+	<h1>다중 파일 업로드(File Drop)</h1>
+	
+	<form method="POST" action="/file/multiaddok.do" enctype="multipart/form-data" id="form3">
+	<table class="vertical">
+		<tr>
+			<th>텍스트</th>
+			<td><input type="text" name="txt" value="홍길동"></td>
+		</tr>
+		<tr>
+			<th>파일</th>
+			<td>
+				<div id="attach-zone"></div>
+				<input type="file" name="attach" id="attach3" style="display:none;">
+				
+			</td>
+		</tr>
+	</table>
+	<div>
+		<button>보내기</button>
+	</div>
+	</form>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <script>
 
-	function checkFile(filename, filesize) {
+	function checkFile(filename, filesize){
+	    const maxsize = 10485760; //10MB
+	    const regex = /^(.*?)\.(exe|sh)$/gi;
 	    
-	    const maxsize = 10485760;	//10MB
-	    const regex = /(.*?)\.(exe|sh)$/gi;
-	    
-	    if (filesize >= maxsize) {
-	        alert('단일 파일의 크기는 10MB 이하만 가능합니다.');
-	        return false;
+	    if(filesize >= maxsize) {
+	       alert('단일 파일의 크기가 10MB 이하만 가능합니다 !')
+	       return false;
 	    }
 	    
-	    if (regex.test(filename)) {	        
-	        alert('해당 파일은 업로드할 수 없습니다.');
-	        return false;	        
+	    if(regex.test(filename)){
+	       alert('해당 파일은 업로드할 수 없습니다.');
+	       return false;
 	    }
 	    
-	    return true;	    
-	}
+	    return true;
+	 }
 
 	//전송되기 바로 직전에 발생하는 이벤트
-	$('form').submit(function() {
+	$('#form1').submit(function() {
 	    
 	    let filename = $('input[name=attach]')[0].files[0].name;
-	    alert(filename);
+	    //alert(filename);
 	    
 	    let filesize = $('input[name=attach]')[0].files[0].size;
-	    alert(filesize);
+	    //alert(filesize);
 	
 	    if(!checkFile(filename, filesize)) {
 	        
@@ -70,6 +129,88 @@
 	    }
 	    
 	});
+	
+	$('#form2').submit(function() {
+	    
+	    //let filename = $('input[name=attach]')[0].files[0].name;	    
+	    //let filesize = $('input[name=attach]')[0].files[0].size;
+	    
+	    let totalsize = 0;
+	    
+	    Array.from($('input[name=attach]')[1].files).forEach(file => {
+	       
+	        if(!checkFile(file.name, file.size)) {    	        
+		        event.preventDefault();
+		        return false;
+		    }
+	        
+	        totalsize += file.size;
+	        
+	    });
+	    
+	    if(totalsize >= 52428800) {
+	        alert('총 파일 크기의 합 50MB 이하만 가능합니다.');
+	        event.preventDefault();
+	        return false;
+	    }
+	    
+	});
+	
+	
+	$('#attach-zone')
+		.on('dragenter', function(e) {
+		    e.preventDefault();
+		    e.stopPropagation();
+		})	
+		.on('dragover', function(e) {
+		    e.preventDefault();
+		    e.stopPropagation();
+		    $(this).css('background-color', 'gold');
+		})	
+		.on('dragleave', function(e) {
+		    e.preventDefault();
+		    e.stopPropagation();
+		    $(this).css('background-color', 'var(--back-color)');
+		})	
+		.on('drop', function(e) {
+		    
+		    $(this).empty();
+		    
+		    e.preventDefault();
+		    
+		    const files = e.originalEvent.dataTransfer.files;
+		    
+		    if(files != null & files != undefined) {
+		        
+		        let temp = '';
+		        
+		        for (let i=0; i<files.length; i++) {
+		            
+		            //console.log(files[i].name);
+		            
+		            let f = files[i];
+		            
+		            let filename = f.name;
+		            let filesize = f.size / 1024 / 1024; //MB 변환
+		            
+		            filesize = filesize < 1 ? filesize.toFixxed(2) : filesize.toFixed(1);
+		            
+		            temp += `
+		            	<div class="item">
+		            		<span>\${filename}</span>
+		            		<span>\${filesize}MB</span>
+		            	</div>
+		            `;
+		            
+		        }// for
+		        
+		        $(this).append(temp);
+		        
+		    }//if
+		    
+		    $(this).css('background-color', 'var(--back-color)');
+		    
+		})	
 
 </script>
 </body>
