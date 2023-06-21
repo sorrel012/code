2fb0f62dbf48acf17c43cb1ab886b28f137a050d
sorrel012 <wain1719@gmail.com>
2023-06-21 12:09:59 +0900

    feat: Create persistent_logins table

--- spring/SecurityTest/script.sql
+++ spring/SecurityTest/script.sql
@@ -54,4 +54,13 @@ select
 from tbl_member m
     left outer join tbl_member_auth a
         on m.userid = a.userid
-            where m.userid = ?
+            where m.userid = ?;
+            
+-----------------------------------------------------------------------------
+
+create table persistent_logins (
+   username varchar(64) not null,
+   series varchar(64) primary key,
+   token varchar(64) not null,
+   last_used timestamp not null
+);
