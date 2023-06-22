1509e2b41743068ca9470fb245c530bfebba9934
sorrel012 <wain1719@gmail.com>
2023-06-22 12:41:10 +0900

    feat: Implement Board DTO and Authorization

--- spring/SecurityBoardTest/src/main/java/com/test/controller/BoardController.java
+++ spring/SecurityBoardTest/src/main/java/com/test/controller/BoardController.java
@@ -1,12 +1,92 @@
 package com.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.test.domain.BoardDTO;
 import com.test.service.BoardService;
 
+@Controller
 public class BoardController {
 
 	@Autowired
 	private BoardService service;
-	
+
+	@GetMapping("/board/list.do")
+	public String list(Model model) {
+
+		model.addAttribute("list", service.list());
+
+		return "board/list";
+
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/board/add.do")
+	public String add() {
+
+		return "board/add";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/board/addok.do")
+	public String addok(BoardDTO dto) {
+		
+		int result = service.add(dto);
+
+		return "redirect:/board/list.do";
+	}
+
+	@GetMapping("/board/view.do")
+	public String view(Model model, String seq) {
+		
+		BoardDTO dto = service.get(seq);
+		
+		model.addAttribute("dto", dto);
+
+		return "board/view";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/board/edit.do")
+	public String edit(Model model, String seq) {
+		
+		BoardDTO dto = service.get(seq);
+		
+		model.addAttribute("dto", dto);
+
+		return "board/edit";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/board/editok.do")
+	public String editok(BoardDTO dto) {
+		
+		int result = service.edit(dto);
+
+		return "redirect:/board/view.do?seq=" + dto.getSeq();
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/board/del.do")
+	public String del(Model model, String seq) {
+		
+		model.addAttribute("seq", seq);
+
+		return "board/del";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/board/delok.do")
+	public String delok(String seq) {
+		
+		int result = service.del(seq);
+
+		return "redirect:/board/list.do";
+	}
+
 }
--- spring/SecurityBoardTest/src/main/webapp/WEB-INF/views/board/add.jsp
+++ spring/SecurityBoardTest/src/main/webapp/WEB-INF/views/board/add.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix = "c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix = "sec" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,6 +14,28 @@
 </head>
 <body>
 
+	<%@ include file="/WEB-INF/views/inc/header.jsp" %>
+	
+	<h2>Board <small>add</small></h2>
+	
+	<form method="POST" action="/board/addok.do">
+	<table class="vertical">
+		<tr>
+			<th>제목</th>
+			<td><input type="text" name="subject" class="full" required></td>
+		</tr>
+		<tr>
+			<th>내용</th>
+			<td><textarea name="content" class="full" required></textarea></td>
+		</tr>
+	</table>
+	<div>
+		<button type="button" class="back" onclick="location.href='/board/list.do';">돌아가기</button>
+		<button type="submit" class="add">글쓰기</button>
+	</div>	
+	<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}">
+	<input type="hidden" name="id" value="<sec:authentication property="principal.username"/>">	
+	</form>	
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <script>
