8cb98360458d0cae760aa8d41204a33506af5205
sorrel012 <wain1719@gmail.com>
2023-06-23 15:04:38 +0900

    feat: Implement chatting system using WebSocket

--- spring/WebSocketTest/src/main/java/com/test/controller/SocketController.java
+++ spring/WebSocketTest/src/main/java/com/test/controller/SocketController.java
@@ -24,4 +24,18 @@ public class SocketController {
 		return "user";
 	}
 	
+	
+	@GetMapping("/index.do")
+	public String index() {
+		
+		return "index";
+	}
+	
+	
+	@GetMapping("/chat.do")
+	public String chat() {
+		
+		return "chat";
+	}
+	
 }
---
+++ spring/WebSocketTest/src/main/java/com/test/domain/ChatDTO.java
@@ -0,0 +1,14 @@
+package com.test.domain;
+
+import lombok.Data;
+
+@Data
+public class ChatDTO {
+
+	private String code;
+	private String sender;
+	private String receiver;
+	private String content;
+	private String regdate;
+	
+}
