01d619b43b1007be1962235bca55dfafa946df7a
sorrel012 <wain1719@gmail.com>
2023-06-26 10:51:50 +0900

    feat: Set Spring Boot project

---
+++ springboot/boot-start/src/main/java/com/test/start/BootStartApplication.java
@@ -0,0 +1,15 @@
+package com.test.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStartApplication {
+
+	public static void main(String[] args) {
+		
+		//스프링 부트 애플리케이션 실행~
+		SpringApplication.run(BootStartApplication.class, args);
+	}
+
+}
